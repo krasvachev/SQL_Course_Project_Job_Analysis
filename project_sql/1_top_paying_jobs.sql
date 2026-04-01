d6a1c5abcf591d509932af43949642ffe354fc61
krasvachev <krasvachev@gmail.com>
2026-04-01 17:51:06 +0300

    Upload 1_top_paying_jobs.sql file

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,24 @@
+/*
+Question: What are the top paying [] roles?
+Find the role according to your interest and need...
+The role could be 'Data Analyst', 'Machine Learning Engineer', etc.
+*/
+
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Machine Learning Engineer' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10
